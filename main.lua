a931f97d4abedd96f1222ae0506aeae99a1ba7f4
rogers492 <rogers492@btinternet.com>
2015-09-28 23:02:58 +0100

    More refactoring - more yet to do

--- main.lua
+++ main.lua
@@ -15,6 +15,8 @@
 ------------------------------------------------------------------------------------------
 
 print ("nexBus is running") -- console output
+local Bow = "Bow Garage"
+local ExD = "Exeter St David's"
 local nexBusErrorText = nil
 local myTextObject1 = display.newText("Hello!", 160, 240, "Arial", 24)
 local myTextObject2 = display.newText("Tap the screen to open a text", 160, 260, "Arial", 16)
@@ -26,18 +28,18 @@ local nexBusErrorTextObj = display.newText(" ", 160, 320, "Arial", 16) -- blank 
 
 function getFromBusStopCode(fromBusStop)  -- arg: fromBusStop -is- the bus stop for which the next few bus departure times are required
 
-	if fromBusStop == "Bow Garage" then
-		local fromBusStopCode = "dvnawdpd"
-	elseif fromBusStop == "Exeter St David's Railway Station" then
-		local fromBusStopCode = "dvndmawa"
+	if fromBusStop == Bow then
+		return "dvnawdpd"
+	elseif fromBusStop == ExD then
+		return "dvndmawa"
 	else
 		nexBusErrorText = "OOPS! BUS STOP NOT RECOGNIZED" -- set the error text
 		return nil
 	end
-	return fromBusStopCode -- fromBusStopCode -is- the 8 letter code for the bus stop we want to depart from
 end
 
 function getBusTimesBySMS(fromBusStopCode)
+print("fTimes:", fromBusStopCode)
 	if fromBusStopCode then -- if fromBusStopCode isn't nil send sms, otherwise do nothing
 		local options = { to = "84268", body = fromBusStopCode } -- { to = phone number, body = bus-stop code }
 		native.showPopup("sms", options )  -- invoke the sms app (default sms app if set, choice of app if no default)
@@ -45,16 +47,23 @@ function getBusTimesBySMS(fromBusStopCode)
 	end
 end
 
--- MAIN PROGRAM --
--- BUTTONTEXT = "Bow Garage"
--- BUTTONTEXT = "Exeter St David's Railway Station"
-BUTTONTEXT = "Unrecognisable stop"
+function getBusTimes
+	print("Only works if the listener method has no args")  -- so you will have to implement buttons and maybe add each fromBusStopCode as a property to each button
+end
 
-display.currentStage:addEventListener("tap", getBusTimesBySMS(getFromBusStopCode(BUTTONTEXT))) -- on screen tap, call getBusTimesBySMS
+-- MAIN PROGRAM --
+BUTTONTEXT = "Bow Garage"
+-- BUTTONTEXT = "Exeter St David's Railway Station"
+-- BUTTONTEXT = "Unrecognisable stop"
+print("BT=", BUTTONTEXT)
+--print("Code=", getFromBusStopCode(BUTTONTEXT))
+print("Error=", nexBusErrorText)
+print("***")
+display.currentStage:addEventListener("tap", getBusTimes ) --(getFromBusStopCode(BUTTONTEXT))) -- on screen tap, call getBusTimesBySMS
 
 if nexBusErrorText then -- nexBusErrorText will be nil if there were no errors
 	local nexBusErrorTextObj = display.newText(nexBusErrorText, 160, 320, "Arial", 16) -- display the error if there is one
-	nexBusErrorTextObj:setFillColor(1,0,0) end -- and set the error text to red
+	nexBusErrorTextObj:setFillColor(1,0,0) -- and set the error text to red
 end
 
 
