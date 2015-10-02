c5117e9c5985e658c0358dc33dfc997f257ad67b
rogers492 <rogers492@btinternet.com>
2015-10-02 22:09:15 +0100

    Tidied informative text

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 -- main.lua -- nexBus -- request bus times from a specific (fixed) bus-stop
 --
 -----------------------------------------------------------------------------------------
--- nexBus v 2.1 2015/10/02 Paul Rogers
+-- nexBus v 2.3 2015/10/02 Paul Rogers
 -- derived from : http://docs.coronalabs.com/api/library/native/showPopup.html#TOC
 
 -- Sends an sms to the transport service sms number
@@ -14,17 +14,17 @@
 -- This reply comes as sms to the sms app - NexBus does no further processing after it opens the sms app.
 ------------------------------------------------------------------------------------------
 
---print ("nexBus is running") -- console output
-
 local nexBusButtonBow = display.newImage("buttonBow.png", display.contentWidth/2, display.contentHeight/2+100)
 local nexBusButtonExD = display.newImage("buttonExD.png", display.contentWidth/2, display.contentHeight/2+200)
 
 local nexBusTextObject1 = display.newText("Tap a button to open a text", display.contentWidth/2, display.contentHeight/2-150, "Arial", 16)
-local nexBusTextObject2= display.newText("that you can send to request bus times", display.contentWidth/2, display.contentHeight/2-100, "Arial", 16)
-local nexBusTextObject3= display.newText("from Bow Garage or Exeter St Davids", display.contentWidth/2, display.contentHeight/2-50, "Arial", 16)
+local nexBusTextObject2 = display.newText("that you can send to request bus times", display.contentWidth/2, display.contentHeight/2-125, "Arial", 16)
+local nexBusTextObject3 = display.newText("from Bow Garage", display.contentWidth/2, display.contentHeight/2-100, "Arial", 18)
+local nexBusTextObject4 = display.newText("or", display.contentWidth/2, display.contentHeight/2-75, "Arial", 16)
+local nexBusTextObject5 = display.newText("Exeter St Davids (Stop A)", display.contentWidth/2, display.contentHeight/2-50, "Arial", 18)
+local nexBusTextObject6 = display.newText("Services 5A and 5B go to Bow - ignore the rest", display.contentWidth/2, display.contentHeight/2-25, "Arial", 14)
 
 function getBusTimesBySMS(fromBusStopCode)
---print("fTimes:", fromBusStopCode)
 	if fromBusStopCode then -- if fromBusStopCode isn't nil send sms, otherwise do nothing
 		local options = { to = "84268", body = fromBusStopCode } -- { to = phone number, body = bus-stop code }
 		native.showPopup("sms", options )  -- invoke the sms app (default sms app if set, choice of app if no default)
@@ -36,7 +36,7 @@ function getBusTimesFromBow(event)
 end
 
 function getBusTimesFromExD(event)
-	getBusTimesBySMS("dvndmawa") -- bus stop code for Exeter St Davids
+	getBusTimesBySMS("dvndmawa") -- bus stop code for Exeter St Davids - Stop A
 end
 
 nexBusButtonBow:addEventListener("tap", getBusTimesFromBow )
